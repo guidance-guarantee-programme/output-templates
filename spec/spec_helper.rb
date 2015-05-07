5ddf24b51dbaa388bb5f2250b8d8bc0c64b24d6a
Gareth Visagie <gareth@gjvis.com>
2015-05-07 12:06:20 +0100

    Add, initialise, and configure Cucumber and RSpec

---
+++ spec/spec_helper.rb
@@ -0,0 +1,20 @@
+require 'output/templates'
+
+# See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
+RSpec.configure do |config|
+  config.expect_with :rspec do |expectations|
+    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
+  end
+
+  config.mock_with :rspec do |mocks|
+    mocks.verify_partial_doubles = true
+  end
+
+  config.default_formatter = 'doc' if config.files_to_run.one?
+  config.disable_monkey_patching!
+  config.profile_examples = 10
+  config.order = :random
+  config.warnings = true
+
+  Kernel.srand config.seed
+end
