52ed00ae004c17cf249b26a5be2b82380d7f3486
Gareth Visagie <gareth@gjvis.com>
2015-05-08 17:25:52 +0100

    Implement tailored guidance contents

--- lib/output/templates/section_selectors/tailored.rb
+++ lib/output/templates/section_selectors/tailored.rb
@@ -2,8 +2,31 @@ module Output
   module Templates
     module SectionSelectors
       class Tailored
-        def call(*)
-          %i(header covering_letter footer)
+        CIRCUMSTANCES = %i(continue_working
+                           unsure
+                           leave_inheritance
+                           wants_flexibility
+                           wants_security
+                           wants_lump_sum
+                           poor_health)
+
+        def call(output_document)
+          [
+            :header,
+            :covering_letter,
+            :introduction,
+            :pension_pot_other,
+            :options_overview,
+            applicable_circumstances(output_document),
+            :other_information,
+            :footer
+          ].flatten
+        end
+
+        private
+
+        def applicable_circumstances(output_document)
+          CIRCUMSTANCES.select { |c| output_document.public_send(c) }
         end
       end
     end
