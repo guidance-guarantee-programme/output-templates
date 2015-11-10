047620bafb252d53e2f75cb64719d95c516a9fba
Andrew Garner <andrew@andrewgarner.com>
2015-11-10 09:26:00 +0000

    Supersede Generic and Tailored templates with Base
    By removing circumstances we no longer need the concept of a Generic or
    Tailored document. They are now the same Base document that in future
    will be supplemented with additional information.

--- spec/templates/section_selectors/tailored_spec.rb
+++
@@ -1,8 +0,0 @@
-RSpec.describe SectionSelectors::Tailored do
-  describe '#call' do
-    let(:income_in_retirement) { 'pension' }
-    let(:output_document) { double('Output Document') }
-
-    subject { described_class.new.call(output_document) }
-  end
-end
