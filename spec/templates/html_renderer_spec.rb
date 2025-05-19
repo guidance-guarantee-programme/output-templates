22ffdc6dccdad6a3a3bab7cd778b24ee3f0883e2
Ben Lovell <benjamin.lovell@gmail.com>
2025-05-19 14:22:34 +0100

    Move existing templates into `en` namespace
    This permits Welsh language templating when the `#welsh?` flag is true
    in the provided data.

--- spec/templates/html_renderer_spec.rb
+++ spec/templates/html_renderer_spec.rb
@@ -3,7 +3,7 @@ require 'securerandom'
 
 RSpec.describe HTMLRenderer do
   describe '#call' do
-    let(:data) { double('Data', test_field: SecureRandom.uuid).as_null_object }
+    let(:data) { double('Data', welsh?: false, test_field: SecureRandom.uuid).as_null_object }
     let(:sections) { %i[section_one section_two section_three] }
     let(:rendered_sections) { "onetwo#{data.test_field}three" } # whitespace stripped, we don't care about it
     let(:asset_root) { Pathname.new(__FILE__).join('..', '..', 'fixtures', 'assets') }
