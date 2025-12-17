327d36b1e781edc9111b2c034780d1af253eeb5e
Ben Lovell <benjamin.lovell@gmail.com>
2025-12-17 12:48:38 +0100

    Revert "Signposting and other PDF changes"

--- spec/templates/section_selectors/ineligible_spec.rb
+++ spec/templates/section_selectors/ineligible_spec.rb
@@ -2,6 +2,6 @@ RSpec.describe SectionSelectors::Ineligible do
   describe '#call' do
     subject { described_class.new.call }
 
-    it { is_expected.to eq(%i[header ineligible scams further_guidance footer]) }
+    it { is_expected.to eq(%i[header ineligible]) }
   end
 end
